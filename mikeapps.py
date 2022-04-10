37063e53a85f1d513f6cf7d8dce8374d5cbcafc7
cowabunga1000 <87511837+cowabunga1000@users.noreply.github.com>
2022-04-10 19:07:48 +0700

    Add files via upload

--- mikeapps.py
+++ mikeapps.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import numpy as np
+from keras.models import load_model
+
 
 st.title("This is Michael's Final Project With Databangalore Indonesia")
 st.subheader("this project is final project match 2")
 
 st.text("project ini memberikan nilai uji untuk data test ASL dari kaggle")
+
+model_inception = load_model("mike_sl.h5")
